a6507cfe174f105eb67bc272d09d22980d78d33a
laves <laves@LAPTOP-CIGH12BJ>
2022-07-02 14:05:19 +0530

    Add project files.

---
+++ VerticalLabel/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace VerticalLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.rotatableLabel1 = new VerticalLabel.RotatableLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rotatableLabel2 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel4 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel5 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel6 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel7 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel3 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel8 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel9 = new VerticalLabel.RotatableLabel();
+            this.rotatableLabel10 = new VerticalLabel.RotatableLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(124, 41);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Regular Control";
+            // 
+            // rotatableLabel1
+            // 
+            this.rotatableLabel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel1.BackgroundImage")));
+            this.rotatableLabel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel1.Location = new System.Drawing.Point(124, 77);
+            this.rotatableLabel1.Name = "rotatableLabel1";
+            this.rotatableLabel1.RotateFlipType = System.Drawing.RotateFlipType.RotateNoneFlipNone;
+            this.rotatableLabel1.Size = new System.Drawing.Size(128, 26);
+            this.rotatableLabel1.TabIndex = 1;
+            this.rotatableLabel1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(266, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Regular Control";
+            // 
+            // rotatableLabel2
+            // 
+            this.rotatableLabel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel2.BackgroundImage")));
+            this.rotatableLabel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel2.Location = new System.Drawing.Point(267, 81);
+            this.rotatableLabel2.Name = "rotatableLabel2";
+            this.rotatableLabel2.RotateFlipType = System.Drawing.RotateFlipType.RotateNoneFlipNone;
+            this.rotatableLabel2.Size = new System.Drawing.Size(144, 22);
+            this.rotatableLabel2.TabIndex = 3;
+            this.rotatableLabel2.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel4
+            // 
+            this.rotatableLabel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel4.BackgroundImage")));
+            this.rotatableLabel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel4.Location = new System.Drawing.Point(55, 159);
+            this.rotatableLabel4.Name = "rotatableLabel4";
+            this.rotatableLabel4.RotateFlipType = System.Drawing.RotateFlipType.Rotate270FlipXY;
+            this.rotatableLabel4.Size = new System.Drawing.Size(34, 170);
+            this.rotatableLabel4.TabIndex = 5;
+            this.rotatableLabel4.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel5
+            // 
+            this.rotatableLabel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel5.BackgroundImage")));
+            this.rotatableLabel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel5.Location = new System.Drawing.Point(24, 159);
+            this.rotatableLabel5.Name = "rotatableLabel5";
+            this.rotatableLabel5.RotateFlipType = System.Drawing.RotateFlipType.Rotate270FlipY;
+            this.rotatableLabel5.Size = new System.Drawing.Size(34, 170);
+            this.rotatableLabel5.TabIndex = 6;
+            this.rotatableLabel5.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel6
+            // 
+            this.rotatableLabel6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel6.BackgroundImage")));
+            this.rotatableLabel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel6.Location = new System.Drawing.Point(85, 107);
+            this.rotatableLabel6.Name = "rotatableLabel6";
+            this.rotatableLabel6.RotateFlipType = System.Drawing.RotateFlipType.Rotate270FlipX;
+            this.rotatableLabel6.Size = new System.Drawing.Size(34, 170);
+            this.rotatableLabel6.TabIndex = 7;
+            this.rotatableLabel6.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel7
+            // 
+            this.rotatableLabel7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel7.BackgroundImage")));
+            this.rotatableLabel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel7.Location = new System.Drawing.Point(112, 109);
+            this.rotatableLabel7.Name = "rotatableLabel7";
+            this.rotatableLabel7.RotateFlipType = System.Drawing.RotateFlipType.Rotate90FlipXY;
+            this.rotatableLabel7.Size = new System.Drawing.Size(34, 170);
+            this.rotatableLabel7.TabIndex = 8;
+            this.rotatableLabel7.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel3
+            // 
+            this.rotatableLabel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel3.BackgroundImage")));
+            this.rotatableLabel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel3.Location = new System.Drawing.Point(266, 159);
+            this.rotatableLabel3.Name = "rotatableLabel3";
+            this.rotatableLabel3.RotateFlipType = System.Drawing.RotateFlipType.RotateNoneFlipNone;
+            this.rotatableLabel3.Size = new System.Drawing.Size(144, 22);
+            this.rotatableLabel3.TabIndex = 9;
+            this.rotatableLabel3.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel8
+            // 
+            this.rotatableLabel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel8.BackgroundImage")));
+            this.rotatableLabel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel8.Location = new System.Drawing.Point(244, 187);
+            this.rotatableLabel8.Name = "rotatableLabel8";
+            this.rotatableLabel8.RotateFlipType = System.Drawing.RotateFlipType.RotateNoneFlipXY;
+            this.rotatableLabel8.Size = new System.Drawing.Size(144, 22);
+            this.rotatableLabel8.TabIndex = 10;
+            this.rotatableLabel8.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel9
+            // 
+            this.rotatableLabel9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel9.BackgroundImage")));
+            this.rotatableLabel9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel9.Location = new System.Drawing.Point(244, 215);
+            this.rotatableLabel9.Name = "rotatableLabel9";
+            this.rotatableLabel9.RotateFlipType = System.Drawing.RotateFlipType.Rotate180FlipY;
+            this.rotatableLabel9.Size = new System.Drawing.Size(144, 22);
+            this.rotatableLabel9.TabIndex = 11;
+            this.rotatableLabel9.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rotatableLabel10
+            // 
+            this.rotatableLabel10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rotatableLabel10.BackgroundImage")));
+            this.rotatableLabel10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rotatableLabel10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rotatableLabel10.Location = new System.Drawing.Point(266, 243);
+            this.rotatableLabel10.Name = "rotatableLabel10";
+            this.rotatableLabel10.RotateFlipType = System.Drawing.RotateFlipType.RotateNoneFlipY;
+            this.rotatableLabel10.Size = new System.Drawing.Size(144, 22);
+            this.rotatableLabel10.TabIndex = 12;
+            this.rotatableLabel10.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(137, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 19);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Possible text rortations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 382);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rotatableLabel10);
+            this.Controls.Add(this.rotatableLabel9);
+            this.Controls.Add(this.rotatableLabel8);
+            this.Controls.Add(this.rotatableLabel3);
+            this.Controls.Add(this.rotatableLabel7);
+            this.Controls.Add(this.rotatableLabel6);
+            this.Controls.Add(this.rotatableLabel5);
+            this.Controls.Add(this.rotatableLabel4);
+            this.Controls.Add(this.rotatableLabel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rotatableLabel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private RotatableLabel rotatableLabel1;
+        private Label label2;
+        private RotatableLabel rotatableLabel2;
+        private RotatableLabel rotatableLabel4;
+        private RotatableLabel rotatableLabel5;
+        private RotatableLabel rotatableLabel6;
+        private RotatableLabel rotatableLabel7;
+        private RotatableLabel rotatableLabel3;
+        private RotatableLabel rotatableLabel8;
+        private RotatableLabel rotatableLabel9;
+        private RotatableLabel rotatableLabel10;
+        private Label label3;
+    }
+}
---
+++ VerticalLabel/RotatableLabel.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+
+namespace VerticalLabel
+{
+    public partial class RotatableLabel : UserControl
+    {
+        public RotatableLabel()
+        {
+            InitializeComponent();
+            _text = string.IsNullOrEmpty(base.Text)?this.Name:base.Text;
+            _rotateFlipType = RotateFlipType.RotateNoneFlipNone;
+            _texAlignment = ContentAlignment.MiddleLeft;
+            this.Size = new Size(150, 21);
+        }
+
+        private string _text;
+        [Browsable(true), EditorBrowsable(EditorBrowsableState.Always),
+            Description("Test text displayed in the textbox"), Category("Data")]
+        public new string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                RotatableLabel_Paint();
+            }
+        }
+
+
+        private RotateFlipType _rotateFlipType;
+        [Browsable(true), EditorBrowsable(EditorBrowsableState.Always),
+         Description("Test text displayed in the textbox"), Category("Data")]
+        public RotateFlipType RotateFlipType
+        {
+            get => _rotateFlipType;
+            set
+            {
+                _rotateFlipType = value;
+                RotatableLabel_Paint();
+            }
+        }
+
+
+        private ContentAlignment _texAlignment;
+        [Browsable(true), EditorBrowsable(EditorBrowsableState.Always),
+         Description("Test text displayed in the textbox"), Category("Data")]
+        public ContentAlignment TextAlignment
+        {
+            get => _texAlignment;
+            set
+            {
+                _texAlignment = value;
+                RotatableLabel_Paint();
+            }
+        }
+
+
+
+        private void RotatableLabel_Paint()
+        {
+
+            switch (_rotateFlipType)
+            {
+                case RotateFlipType.RotateNoneFlipX:
+                case RotateFlipType.RotateNoneFlipXY:
+                case RotateFlipType.RotateNoneFlipY:
+                case RotateFlipType.RotateNoneFlipNone:
+                    var horizontalBitmap = new Bitmap(this.Width, this.Height);
+                    var horizontalLabel = new Label() { Text = _text, Font = this.Font, Size = this.Size, TextAlign = TextAlignment,RightToLeft = this.RightToLeft};
+                    horizontalLabel.DrawToBitmap(horizontalBitmap, new Rectangle(0, 0, this.Width, this.Height));
+                    horizontalLabel.Dispose();
+                    horizontalBitmap.RotateFlip(_rotateFlipType);
+                    this.BackgroundImage = horizontalBitmap;
+                    this.BackgroundImageLayout = ImageLayout.Stretch;
+                    break;
+                case RotateFlipType.Rotate270FlipX:
+                case RotateFlipType.Rotate270FlipXY:
+                case RotateFlipType.Rotate270FlipY:
+                case RotateFlipType.Rotate270FlipNone:
+                    var verticalBitmap = new Bitmap(this.Height, this.Width);
+                    var verticalLabel = new Label() { Text = _text, Font = this.Font, AutoSize = true, Size = new Size(this.Height, this.Width), TextAlign = TextAlignment,RightToLeft = this.RightToLeft};
+                    verticalLabel.DrawToBitmap(verticalBitmap, new Rectangle(0, 0, this.Height, this.Width));
+                    verticalLabel.Dispose();
+                    verticalBitmap.RotateFlip(_rotateFlipType);
+                    this.BackgroundImage = verticalBitmap;
+                    this.BackgroundImageLayout = ImageLayout.Stretch;
+                    break;
+
+            }
+        }
+
+        private void RotatableLabelControl_FontChanged(object sender, EventArgs e)
+        {
+            RotatableLabel_Paint();
+        }
+
+        private void RotatableLabelControl_SizeChanged(object sender, EventArgs e)
+        {
+            RotatableLabel_Paint();
+        }
+    }
+}
